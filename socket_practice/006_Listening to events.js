35bebea9ce16f1ba83946eb8f936d0cf7e607aa6
shpark0913 <shpark097754@gmail.com>
2023-08-28 16:13:20 +0900

    memo: Study about "Catch-all listeners"

--- socket_practice/006_Listening to events.js
+++ socket_practice/006_Listening to events.js
@@ -1,5 +1,5 @@
 // 서버와 클라이언트 간 전송되는 events를 다루는 몇 가지의 방법이 있다.
-// EventEmitter methods
+// EventEmitter methods /////////////////////////////////////////////////////
 
 // socket.on(evnetName, listener)
 // eventName이라는 이벤트에 대한 리스너 배열 끝에 리스너 함수를 추가한다.
@@ -30,5 +30,28 @@ socket.removeAllListeners("details");
 socket.removeAllListeners();
 
 
+// Catch-all listeners /////////////////////////////////////////////////////
 
-// https://socket.io/docs/v4/listening-to-events/ 의 Catch-all listeners 부터 마저 학습
+// socket.onAny(listener)
+// 이벤트가 발생하면 실행될 리스너를 등록한다.
+socket.onAny((eventName, ...args) => {
+  // ...
+})
+
+// socket.prependAny(listener)
+// 어떤 이벤트가 emit되어도 실행될 리스너를 등록하라. 이 리스너는 리스너 배열의 초반에 더해져야 한다.
+socket.prependAny((eventName, ...args) => {
+  // ...
+})
+
+// socket.offAny([listener])
+// 모든 catch-all 리스너들 혹은 지정된 리스너를 제거한다.
+const listener2 = (eventName, ...args) => {
+  console.log(eventName, args);
+}
+socket.onAny(listener2);
+// and then later...
+socket.offAny(listener2);
+// or all listeners
+socket.offAny();
+
